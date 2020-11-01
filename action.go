7a35ac7609dadac8cf5392a221e0c58b46ecc0d9
thanh phu <htp1912@gmail.com>
2020-11-01 19:38:48 +0700

    refactor

--- action.go
+++ action.go
@@ -1,25 +1,16 @@
 package security
 
 const (
-	ActionNone     int32 = 0
-	ActionView     int32 = 1
-	ActionSearch   int32 = 2
-	ActionQueryAll int32 = 4
-	ActionViewAll  int32 = 256 - 1
-	ActionAdd      int32 = 256
-	ActionEdit     int32 = 512
-	ActionPatch    int32 = 1024
-	ActionApprove  int32 = 2048
-	ActionReject   int32 = 4096
-	ActionDelete   int32 = 32768
-	ActionWriteAll int32 = 65536 - 1
-	ActionAll      int32 = 2147483647
+	ActionNone    int32 = 0
+	ActionRead    int32 = 1
+	ActionWrite   int32 = 2
+	ActionApprove int32 = 4
+	ActionDelete  int32 = 8
+	ActionAll     int32 = 2147483647
 
-	ActionViewAllView        int32 = ActionViewAll | ActionView
-	ActionSearchView         int32 = ActionSearch | ActionView
-	ActionAddEdit            int32 = ActionAdd | ActionEdit
-	ActionAddEditPatch       int32 = ActionAdd | ActionEdit | ActionPatch
-	ActionEditPatch          int32 = ActionEdit | ActionPatch
-	ActionApproveReject      int32 = ActionApprove | ActionReject
-	ActionAddEditPatchDelete int32 = ActionAdd | ActionEdit | ActionPatch | ActionDelete
+	ActionReadWrite              int32 = ActionRead | ActionWrite
+	ActionReadWriteApprove       int32 = ActionRead | ActionWrite | ActionApprove
+	ActionReadWriteApproveDelete int32 = ActionRead | ActionWrite | ActionApprove | ActionDelete
+	ActionReadApprove            int32 = ActionRead | ActionApprove
+	ActionReadApproveDelete      int32 = ActionRead | ActionApprove | ActionDelete
 )
