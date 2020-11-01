ff19f5322bdccdddf96644d5e4c0c9c43539ee27
thanh phu <htp1912@gmail.com>
2020-11-01 15:17:47 +0700

    Add SqlPrivilegeLoader

---
+++ sql_privilege_loader.go
@@ -0,0 +1,26 @@
+package security
+
+import (
+	"context"
+	"database/sql"
+)
+
+type SqlPrivilegeLoader struct {
+	DB    *sql.DB
+	Query string
+}
+
+func NewSqlPrivilegeLoader(db *sql.DB, query string) *SqlPrivilegeLoader{
+	return &SqlPrivilegeLoader{DB: db, Query: query}
+}
+func (l SqlPrivilegeLoader) Privilege(ctx context.Context, userId string, privilegeId string) int32 {
+	var permissions int32 = 0
+	err := l.DB.QueryRow(l.Query, userId, privilegeId).Scan(&permissions)
+	if err != nil {
+		return 0
+	}
+	if permissions == 0 {
+		return 1
+	}
+	return permissions
+}
---
+++ sql_privileges_loader.go
@@ -0,0 +1,35 @@
+package security
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type SqlPrivilegesLoader struct {
+	DB    *sql.DB
+	Query string
+}
+
+func NewSqlPrivilegesLoader(db *sql.DB, query string) *SqlPrivilegesLoader {
+	return &SqlPrivilegesLoader{DB: db, Query: query}
+}
+
+func (l SqlPrivilegesLoader) Privileges(ctx context.Context, userId string) []string {
+	privileges := make([]string, 0)
+	rows, err := l.DB.Query(l.Query, userId)
+	if err != nil {
+		return privileges
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id string
+		var permissions int32
+		if err = rows.Scan(&id, &permissions); err == nil {
+			x := id + " " + fmt.Sprintf("%X", permissions)
+			privileges = append(privileges, x)
+		}
+	}
+	return privileges
+}
