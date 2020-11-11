502dad5b4bd0b9bf83f708692b6aab9c8e5149d8
thanh phu <htp1912@gmail.com>
2020-11-11 19:32:15 +0700

    privilegeLoader support more driver name

--- sql_privilege_loader.go
+++ sql_privilege_loader.go
@@ -3,6 +3,17 @@ package security
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+const (
+	DriverPostgres   = "postgres"
+	DriverMysql      = "mysql"
+	DriverMssql      = "mssql"
+	DriverOracle     = "oracle"
+	DriverNotSupport = "no support"
 )
 
 type SqlPrivilegeLoader struct {
@@ -10,9 +21,17 @@ type SqlPrivilegeLoader struct {
 	Query string
 }
 
-func NewSqlPrivilegeLoader(db *sql.DB, query string) *SqlPrivilegeLoader{
+func NewPrivilegeLoader(db *sql.DB, query string) *SqlPrivilegeLoader {
+	driver := GetDriver(db)
+	query = replaceQueryparam(driver, query)
+	return NewSqlPrivilegeLoader(db, query, driver)
+}
+
+func NewSqlPrivilegeLoader(db *sql.DB, query string, driver string) *SqlPrivilegeLoader {
+	query = replaceQueryparam(driver, query)
 	return &SqlPrivilegeLoader{DB: db, Query: query}
 }
+
 func (l SqlPrivilegeLoader) Privilege(ctx context.Context, userId string, privilegeId string) int32 {
 	var permissions int32 = 0
 	err := l.DB.QueryRow(l.Query, userId, privilegeId).Scan(&permissions)
@@ -23,4 +42,35 @@ func (l SqlPrivilegeLoader) Privilege(ctx context.Context, userId string, privil
 		return ActionAll
 	}
 	return permissions
+}
+
+func replaceQueryparam(driver string, query string) string {
+	if driver == DriverOracle || driver == DriverPostgres {
+		var x string
+		if driver == DriverOracle {
+			x = ":val"
+		} else {
+			x = "$"
+		}
+		for i := 1; i < 3; i++ {
+			query = strings.Replace(query, "?", x+fmt.Sprintf("%v", i), 1)
+		}
+	}
+	return query
+}
+
+func GetDriver(db *sql.DB) string {
+	driver := reflect.TypeOf(db.Driver()).String()
+	switch driver {
+	case "*pq.Driver":
+		return DriverPostgres
+	case "*mysql.MySQLDriver":
+		return DriverMysql
+	case "*mssql.Driver":
+		return DriverMssql
+	case "*godror.drv":
+		return DriverOracle
+	default:
+		return DriverNotSupport
+	}
 }
