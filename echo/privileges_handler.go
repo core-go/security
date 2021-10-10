1bfcd746516091878b82c385a937a1368f871584
Duc Nguyen <minhduc140583@gmail.com>
2021-10-10 17:16:30 +0700

    Refactor respond JSON

--- echo/privileges_handler.go
+++ echo/privileges_handler.go
@@ -17,47 +17,43 @@ func NewPrivilegeHandler(privilegeLoader func(ctx context.Context, userId string
 	return &PrivilegesHandler{Privilege: privilegeLoader, Privileges: privilegesLoader}
 }
 
-func (h *PrivilegesHandler) GetPrivileges() echo.HandlerFunc {
-	return func(ctx echo.Context) error {
-		r := ctx.Request()
-		id := ""
-		if r.Method == "GET" {
-			i := strings.LastIndex(r.RequestURI, "/")
-			if i >= 0 {
-				id = r.RequestURI[i+1:]
-			}
-		} else {
-			b, er1 := ioutil.ReadAll(r.Body)
-			if er1 != nil {
-				return ctx.String(http.StatusBadRequest, "Require id")
-			}
-			id = strings.Trim(string(b), " ")
+func (h *PrivilegesHandler) GetPrivileges(ctx echo.Context) error {
+	r := ctx.Request()
+	id := ""
+	if r.Method == "GET" {
+		i := strings.LastIndex(r.RequestURI, "/")
+		if i >= 0 {
+			id = r.RequestURI[i+1:]
 		}
-		if len(id) == 0 {
+	} else {
+		b, er1 := ioutil.ReadAll(r.Body)
+		if er1 != nil {
 			return ctx.String(http.StatusBadRequest, "Require id")
 		}
-		result := h.Privileges(r.Context(), id)
-		return ctx.JSON(http.StatusOK, result)
+		id = strings.Trim(string(b), " ")
 	}
+	if len(id) == 0 {
+		return ctx.String(http.StatusBadRequest, "Require id")
+	}
+	result := h.Privileges(r.Context(), id)
+	return ctx.JSON(http.StatusOK, result)
 }
-func (h *PrivilegesHandler) GetPrivilege() echo.HandlerFunc {
-	return func(ctx echo.Context) error {
-		r := ctx.Request()
-		s := strings.Split(r.RequestURI, "/")
-		if len(s) < 3 {
-			return ctx.String(http.StatusBadRequest, "URL is not valid")
-		}
-
-		if r.Method != "GET" {
-			return ctx.String(http.StatusBadRequest, "Must use GET method")
-		}
-		userId := s[len(s)-2]
-		privilegeId := s[len(s)-1]
-		if len(userId) == 0 || len(privilegeId) == 0 {
-			return ctx.String(http.StatusBadRequest, "parameters cannot be empty")
-
-		}
-		result := h.Privilege(r.Context(), userId, privilegeId)
-		return ctx.JSON(http.StatusOK, result)
+func (h *PrivilegesHandler) GetPrivilege(ctx echo.Context) error {
+	r := ctx.Request()
+	s := strings.Split(r.RequestURI, "/")
+	if len(s) < 3 {
+		return ctx.String(http.StatusBadRequest, "URL is not valid")
 	}
+
+	if r.Method != "GET" {
+		return ctx.String(http.StatusBadRequest, "Must use GET method")
+	}
+	userId := s[len(s)-2]
+	privilegeId := s[len(s)-1]
+	if len(userId) == 0 || len(privilegeId) == 0 {
+		return ctx.String(http.StatusBadRequest, "parameters cannot be empty")
+
+	}
+	result := h.Privilege(r.Context(), userId, privilegeId)
+	return ctx.JSON(http.StatusOK, result)
 }
